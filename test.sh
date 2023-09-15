5bd61c71e30c3b6ed37c28a768fd3dc23d983fef
thotsbay2023 <131166579+thotsbay@users.noreply.github.com>
2023-09-15 12:01:50 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,6 +5,20 @@ GO_VERSION="1.20.8"
 sudo apt-get update
 sudo apt-get install -y upx-ucl curl unzip gcc-aarch64-linux-gnu devscripts build-essential
 
+latest_release_info=$(curl -s https://api.github.com/repos/cloudflare/cloudflared/releases/latest)
+latest_version=$(echo "$latest_release_info" | grep '"tag_name":' | cut -d '"' -f 4)
+
+current_version=$(curl -s "https://raw.githubusercontent.com/thotsbay/blog/main/README.md" | grep 'Latest Version:' | awk '{print $NF}')
+
+if [ "$latest_version" != "$current_version" ]; then
+  echo "Remote Cloudflared version ($current_version) is different from latest version ($latest_version). Updating..."
+  update_readme
+  download_compile_upload
+  build_deb_package
+else
+  echo "Remote Cloudflared version is up to date. No need to download and compile gost."
+fi
+
 update_readme() {
   cat <<EOL > README.md
 Latest Version: $latest_version
@@ -56,23 +70,8 @@ Description: $DESCRIPTION
 EOF
 
   dpkg-deb --build "$PACKAGE_DIR"
+
   rm -rf "$PACKAGE_DIR"
-  
-  echo "软件包 $PACKAGE_NAME.deb 创建完成。"
+
+  echo "Package $PACKAGE_NAME.deb created successfully."
 }
-
-latest_release_info=$(curl -s https://api.github.com/repos/cloudflare/cloudflared/releases/latest)
-latest_version=$(echo "$latest_release_info" | grep '"tag_name":' | cut -d '"' -f 4)
-
-current_version=$(curl -s "https://raw.githubusercontent.com/thotsbay/blog/main/README.md" | grep 'Latest Version:' | awk '{print $NF}')
-
-if [ "$latest_version" != "$current_version" ]; then
-  echo "Remote Cloudflared version ($current_version) is different from latest version ($latest_version). Updating..."
-
-  update_readme
-  download_compile_upload
-  build_deb_package
-
-else
-  echo "Remote Cloudflared version is up to date. No need to download and compile gost."
-fi
