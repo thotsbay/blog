deaffcb08eb8e8cfb579384d7b0cbfef58a600b6
thotsbay2023 <131166579+thotsbay@users.noreply.github.com>
2023-09-17 21:14:38 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,39 +1,29 @@
 #!/bin/bash
 
-# 设置变量
-MYSQL_VERSION="8.0.34"
-ARCH="amd64"
-PKG_NAME="mysql-${MYSQL_VERSION}-${ARCH}"
+PACKAGE_NAME="mysql"
+PACKAGE_VERSION="8.0.34"
+PACKAGE_ARCH="amd64"
+PACKAGE_FILENAME="${PACKAGE_NAME}-${PACKAGE_VERSION}-${PACKAGE_ARCH}.deb"
+DESCRIPTION="MySQL Database Server"
 
-# 创建工作目录
-WORK_DIR="/tmp/${PKG_NAME}"
-mkdir -p "$WORK_DIR/DEBIAN"
-mkdir -p "$WORK_DIR/usr/bin"
+PACKAGE_DIR="$PACKAGE_NAME-$PACKAGE_VERSION-$PACKAGE_ARCH"
+DEBIAN_DIR="$PACKAGE_DIR/DEBIAN"
+INSTALL_DIR="$PACKAGE_DIR/usr/bin"
 
-# 复制 MySQL 二进制文件到目标目录
-cp mysql "$WORK_DIR/usr/bin/mysql"
+mkdir -p "$DEBIAN_DIR"
+chmod 0755 "$DEBIAN_DIR"
 
-# 创建控制文件 (control file)
-cat <<EOF > "$WORK_DIR/DEBIAN/control"
-Package: $PKG_NAME
-Version: $MYSQL_VERSION
-Section: database
-Priority: optional
-Architecture: $ARCH
-Maintainer: Your Name <your.email@example.com>
-Description: MySQL Database Server
+mkdir -p "$INSTALL_DIR"
+cp mysql "$INSTALL_DIR"
+
+cat > "$DEBIAN_DIR/control" <<EOF
+Package: $PACKAGE_NAME
+Version: $PACKAGE_VERSION
+Architecture: $PACKAGE_ARCH
+Description: $DESCRIPTION
 EOF
 
-# 设置权限和所有者
-chmod 755 "$WORK_DIR/DEBIAN/control"
-
-# 使用 dpkg-deb 创建软件包
-dpkg-deb --build "$WORK_DIR" "$PKG_NAME.deb"
-
-# 移动软件包到当前目录
-mv "$PKG_NAME.deb" .
-
-# 清理临时目录
-rm -rf "$WORK_DIR"
-
-echo "MySQL Debian 软件包已创建：$PKG_NAME.deb"
+dpkg-deb --build "$PACKAGE_DIR" "$PACKAGE_FILENAME.deb"
+rm -rf "$PACKAGE_DIR"
+  
+echo "Package $PACKAGE_FILENAME.deb created successfully."
